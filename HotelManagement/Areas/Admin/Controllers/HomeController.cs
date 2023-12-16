9ccb1f673f84d10fda3251483caa8151a3128e8c
PhatDongNai <phatnguyen5523@gmail.com>
2023-12-16 22:25:18 +0700

    change Dashboard 1

--- HotelManagement/Areas/Admin/Controllers/HomeController.cs
+++ HotelManagement/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,22 @@ namespace HotelManagement.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        Hotel_ManagementEntities db = new Hotel_ManagementEntities();
         // GET: Admin/Home
         public ActionResult Index()
         {
+            var totalRooms = db.Phongs.Count();
+            var totalEmployees = db.NhanViens.Count();
+            var totalBookings = db.PhieuThuePhongs.Count();
+            var totalRoomBookings = db.Phongs.Count(p => p.HienTrang == true);
+            var totalRevenue = db.HoaDons.Sum(h => h.TienPhong + (h.TienDichVu ?? 0));
+            string formattedNumber = totalRevenue.ToString("#,##0");
+
+            ViewBag.TotalRooms = totalRooms;
+            ViewBag.TotalEmployees = totalEmployees;
+            ViewBag.TotalBookings = totalBookings;
+            ViewBag.TotalRoomBookings = totalRoomBookings;
+            ViewBag.TotalRevenue = formattedNumber;
             return View();
         }
         public ActionResult Rooms()
@@ -44,6 +58,35 @@ namespace HotelManagement.Areas.Admin.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        public JsonResult GetMonthlyEarning()
+        {
+            var monthlyEarning = db.HoaDons
+                .GroupBy(h => new { Year = h.NgayLapHoaDon.Year, Month = h.NgayLapHoaDon.Month })
+                .Select(g => new
+                {
+                    Year = g.Key.Year,
+                    Month = g.Key.Month,
+                    TotalEarning = g.Sum(h => h.TienPhong + (h.TienDichVu ?? 0))
+                })
+                .OrderBy(g => g.Year)
+                .ThenBy(g => g.Month)
+                .ToList();
+            return Json(monthlyEarning, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetYearlyEarning()
+        {
+            var yearlyEarning = db.HoaDons
+                .GroupBy(h => h.NgayLapHoaDon.Year)
+                .Select(g => new
+                {
+                    Year = g.Key,
+                    TotalEarning = g.Sum(h => h.TienPhong + (h.TienDichVu ?? 0))
+                })
+                .OrderBy(g => g.Year)
+                .ToList();
+            return Json(yearlyEarning, JsonRequestBehavior.AllowGet);
         }
     }
 }
